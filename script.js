8503bbc62a4d692d273aee5cbffacb79a41142a7
nayma092 <nayma092@gmail.com>
2024-11-15 07:44:56 +0600

    js-colorbox done

---
+++ script.js
@@ -0,0 +1,46 @@
+let main=document.querySelector(".main")
+let one=document.querySelector(".one")
+let two=document.querySelector(".two")
+let three=document.querySelector(".three")
+let four=document.querySelector(".four")
+let five=document.querySelector(".five")
+let six=document.querySelector(".six")
+let seven=document.querySelector(".seven")
+let eight=document.querySelector(".eight")
+let nine=document.querySelector(".nine")
+let ten=document.querySelector(".ten")
+let reset=document.querySelector(".reset")
+
+one.addEventListener("click",function(){
+    main.classList.add("one");
+});
+two.addEventListener("click",function(){
+    main.classList.add("two");
+});
+three.addEventListener("click",function(){
+    main.classList.add("three");
+});
+four.addEventListener("click",function(){
+    main.classList.add("four");
+});
+five.addEventListener("click",function(){
+    main.classList.add("five");
+});
+six.addEventListener("click",function(){
+    main.classList.add("six");
+});
+seven.addEventListener("click",function(){
+    main.classList.add("seven");
+});
+eight.addEventListener("click",function(){
+    main.classList.add("eight");
+});
+nine.addEventListener("click",function(){
+    main.classList.add("nine");
+});
+ten.addEventListener("click",function(){
+    main.classList.add("ten");
+});
+reset.addEventListener("click",function(){
+    main.classList.add("reset");
+});
